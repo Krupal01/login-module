2afac72ed1683f8513fd64c98589d3f2478d1da4
KrupalTatva <tatvasoft.krupal@gmail.com>
2023-10-26 09:59:07 +0530

    add basic widget

--- lib/login_module.dart
+++ lib/login_module.dart
@@ -1,6 +1,36 @@
 library login_module;
 
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class CounterWidget extends StatefulWidget {
+  const CounterWidget({super.key});
+
+  @override
+  State<CounterWidget> createState() => _CounterWidgetState();
+}
+
+class _CounterWidgetState extends State<CounterWidget> {
+  var count = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Container(
+       padding: EdgeInsets.all(5),
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(5)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(count.toString(), style: TextStyle(fontSize: 24, color: Colors.black),),
+            const SizedBox(height: 10,),
+            ElevatedButton(onPressed: (){
+              setState(() {
+                count = count+1;
+              });
+            }, child: const Text("add"))
+          ],
+        ),
+      ),
+    );
+  }
 }
